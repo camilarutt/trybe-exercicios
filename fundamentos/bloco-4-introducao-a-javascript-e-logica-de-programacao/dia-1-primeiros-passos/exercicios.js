968d3d893e4474e16b6e704837b14c0ca3a3743e
Camila Ruttimann <camilarut@gmail.com>
2022-08-25 19:59:05 -0300

    Exercicios 4.1 - Parte 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/exercicios.js
@@ -0,0 +1,11 @@
+const a = 5;
+const b = 4;
+const c = 3;
+
+const adicao = a + b;
+const subtracao = a - b;
+const multiplicacao = a * b;
+const divisao = a / b;
+const modulo = a % b;
+
+console.log(adicao, subtracao, multiplicacao, divisao, modulo);
