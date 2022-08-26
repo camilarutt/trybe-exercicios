142dc9b3b026b44d6edc5dbbb5a7153d5b4be18b
Camila Ruttimann <camilarut@gmail.com>
2022-08-26 16:45:05 -0300

    Exercicios 4.3 - Logica de Programação

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-progamacao-e-algoritmos/exercicio2.js
@@ -0,0 +1,8 @@
+var numero = 10;
+var resultado = numero; 
+
+for (index = 1; index < numero; index += 1) {
+    resultado *= index;
+}
+
+console.log(resultado);
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-progamacao-e-algoritmos/exercicios.js
@@ -0,0 +1,8 @@
+const n = 100 ;
+
+let resultado = 1;
+for (let index = 0; index <= n; index += 1) {
+  resultado += index;
+};
+console.log(resultado);
+
