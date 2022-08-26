1b712764fd95b25f94017222a3231ddb960bfe08
Camila Ruttimann <camilarut@gmail.com>
2022-08-26 20:47:22 -0300

    exercicios bonus - quadrado e estrelas

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-progamacao-e-algoritmos/geometrics.js
@@ -0,0 +1,8 @@
+let n = 5;
+let asterisco = "";
+let simbolo = "*";
+
+    for (index = 0; index < n ; index +=1) {
+    let linha = simbolo.repeat(5);
+    console.log(linha);
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-progamacao-e-algoritmos/piramide.js
@@ -0,0 +1,7 @@
+let n = 5;
+let simbolo = "*";
+
+    for (index = 0; index <= n ; index +=1) {
+    let linha = simbolo.repeat(index);
+    console.log(linha);
+}
