e57ace6742467e90d6e318e6fa8047b1727d69ca
Camila Ruttimann <camilarut@gmail.com>
2022-08-25 21:37:11 -0300

    Exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
@@ -1,16 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
+let resultado = numbers[0];
 
-for (let index = 0; index < numbers.length; index += 1) {
-    resultado += numbers[index];
-  }
-  
-let mediaArit = resultado / numbers.length + 1;
-
-  if (mediaArit > 20) {
-    console.log("Valor maior que 20");
+for (let index = 1; index < numbers.length; index += 1) {
+   if (numbers[index] > resultado) {
+    resultado = numbers[index];
+   }
   }
 
-  else {
-    console.log("Valor menor ou igual a 20")
-  }
+console.log(resultado);
