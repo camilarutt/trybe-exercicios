2bb28cd793b5d92d2981e14ccf0b63320615a803
Camila Ruttimann <camilarut@gmail.com>
2022-08-25 21:19:15 -0300

    Exercicio 3 - 4.3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios.js
@@ -1,5 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
 
-for (let numbo of numbers) {
-    console.log(numbo);
-}
+for (let index = 0; index < numbers.length; index += 1) {
+    resultado += numbers[index];
+  }
+  
+  console.log(resultado / numbers.length + 1);
