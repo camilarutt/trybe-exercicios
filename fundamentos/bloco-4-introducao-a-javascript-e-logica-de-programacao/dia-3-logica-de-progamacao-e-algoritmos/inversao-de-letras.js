40cbf70a72d27fc6b1704019d24a3ec5b39f7996
Camila Ruttimann <camilarut@gmail.com>
2022-08-26 23:22:47 -0300

    Primeiras funções - Exercícios Parte II - 4.4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-progamacao-e-algoritmos/inversao-de-letras.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-progamacao-e-algoritmos/inversao-de-letras.js
@@ -1,10 +1,6 @@
 let word = 'tryber';
 let reverseWord = '';
 
-for (let index = 0; index < word.length; index += 1) {
-  reverseWord += word[word.length - 1 - index];
-}
-
 reverseWord = word.split('').reverse().join(reverseWord);
 
 console.log(reverseWord);
